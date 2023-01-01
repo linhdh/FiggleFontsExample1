471df807d8a97e466cf5549c859b201867f62481
Linh Dao <linhdh@live.com>
2023-01-01 17:38:33 +0700

    Figgle font example

---
+++ FiggleFontExample1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Figgle;
+
+namespace FiggleFontExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render("Hello, World!"));
+        }
+    }
+}
